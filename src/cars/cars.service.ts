7fd50aeba65367381bb99dd144062e8a5edfef49
Lucas Regalado <lucasregalado@hotmail.com>
2025-08-06 16:08:14 -0300

    Create a new car

--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { Car } from './interfaces/car.interface';
 import { v4 as uuidv4 } from 'uuid';
+import { CreateCarDto } from './dto/create-car.dto';
 
 @Injectable()
 export class CarsService {
@@ -30,6 +31,24 @@ export class CarsService {
     const car = this.cars.find((car) => car.id === id);
 
     if (!car) throw new NotFoundException(`Car with id '${id}' not found`);
+
+    return car;
+  }
+
+  create(createCarDto: CreateCarDto) {
+    const isCar = this.findAll().some(
+      (car) =>
+        car.brand === createCarDto.brand && car.model === createCarDto.model,
+    );
+
+    if (!isCar) throw new NotFoundException(`Your car already exists`);
+
+    const car: Car = {
+      id: uuidv4(),
+      ...createCarDto,
+    };
+
+    this.cars.push(car);
 
     return car;
   }
