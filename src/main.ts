b8a10e6200b53ce4586978630f0351742287b114
Lucas Regalado <lucasregalado@hotmail.com>
2025-08-06 15:55:16 -0300

    Use global pipes

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { ValidationPipe } from '@nestjs/common';
+
 async function bootstrap() {
   //we can change the name for main
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
